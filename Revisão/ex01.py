a94a82625797484cf6dc05bd4bafc4eebe47f528
Anderson0312 <andersonmoura812@gmail.com>
2022-03-23 19:13:54 -0300

    Create ex01.py

---
+++ Revisão/ex01.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um numero:'))
+n2 = int(input('Digite outro numero:'))
+
+if n1 >= n2:
+    print('O {} é maior que {}! '.format(n1, n2))
+else:
+    print('O {} é menor que {}! '.format(n1, n2))   
